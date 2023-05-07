622b4215050a45f796b3d04bc3019966d4f4014a
Gal Malka <73529828+galmalka122@users.noreply.github.com>
2023-05-07 14:12:01 +0300

    vector class files

---
+++ tasks_week1_Gal_Malka/include/VectorTests.h
@@ -0,0 +1,16 @@
+#include<cstdlib>
+#include<time.h>
+#include<vector>
+#include"Vector.h"
+
+bool pop_back_test();
+bool push_back_test();
+bool swap_test();
+bool resize_test();
+bool clear_test();
+bool size_test();
+bool data_test();
+bool empty_test();
+bool operators_test();
+bool insert_test();
+bool erase_test();
